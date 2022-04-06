578f2fae0b8bd067615e258e1b518a50ebca7d5c
MultiFlex <wakanawardrobe@gmail.com>
2022-04-06 14:53:25 +0100

    use for each to add full grid

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 
-// const container = document.querySelector('.container');
 
-// newDiv = document.createElement('div');
+function createRow() {
+    
+    const container = document.querySelector('.container');
+    subContainer = document.createElement('div');
+    subContainer.className = "sub-container";
 
-// container.appendChild(newDiv);
+    for (i=0; i<16; i++) {
 
-// newDiv.textContent = "wassup";
+        smallBox = document.createElement('div');
+        smallBox.id = "box";
 
-for (i=0; i<16; i++) {
-    newDiv = document.createElement('div');
-    newDiv.id = "box";
-
-    document.querySelector('.container').appendChild(newDiv);
-    newDiv.textContent = "wassup"
+        subContainer.appendChild(smallBox);
+    }
+    container.appendChild(subContainer);
 }
+
+function fullGrid() {
+    myRows = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+
+    myRows.forEach(() => {
+        createRow();
+    });
+}
+
+fullGrid();
