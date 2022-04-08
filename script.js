08e39e90d90c33a77fb8a7ac88e324250025b4f2
MultiFlex <wakanawardrobe@gmail.com>
2022-04-08 15:51:36 +0100

    remove container child using innerhtml

--- script.js
+++ script.js
@@ -1,59 +1,48 @@
-const container = document.querySelector('.container');
 const button = document.querySelector('button');
 
-// function resetState() {
-//     container.removeChild(subContainer);
-// }
+const container = document.querySelector('.container');
+
+button.addEventListener('click', fullGrid);
 
 function createRow(input) {
-    
     subContainer = document.createElement('div');
     subContainer.className = "sub-container";
 
+
     for (i=0; i < input; i++) {
 
-        smallBox = document.createElement('div');
-        smallBox.id = "box";
-
+        let smallBox = document.createElement('div');
+        smallBox.className = "box";
         subContainer.appendChild(smallBox);
 
     }
 
     container.appendChild(subContainer);
-
-
 }
 
 
 function fullGrid() {
+    container.innerHTML = "";
     let userInput = prompt('Pick a number for the grid: ');
     let myNewArray = [];
 
     for (i=1; i <= userInput; i++) {
-        //mynewArray.push[i];
         myNewArray.push([i]);
     }
 
-    //myRows = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
-
+    
     myNewArray.forEach(() => {
         createRow(userInput);
     });
-}
 
-fullGrid();
-
-button.addEventListener('click', clearBoard);
-
-
-const boxes = document.querySelectorAll('#box');
-Array.from(boxes).forEach((box) => {
-    box.addEventListener('mouseover', () => {
-        box.style.backgroundColor = getRandomColor();
+    const boxes = document.querySelectorAll('.box');
+    
+    Array.from(boxes).forEach((box) => {
+        box.addEventListener('mouseover', () => {
+            box.style.backgroundColor = getRandomColor();
+        });
     });
-});
 
-function clearBoard() {
     Array.from(boxes).forEach((box) => {
         box.style.backgroundColor = 'white';
     });
