bc37511bd230f033d356d06548f1bd035ce46c21
MultiFlex <wakanawardrobe@gmail.com>
2022-04-09 15:38:44 +0100

    add a title after player input

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
 const button = document.querySelector('.button');
 const container = document.querySelector('.container');
-
+const heading = document.querySelector('h1');
 button.addEventListener('click', fullGrid);
 
 button.addEventListener('mouseover', () => {
     button.classList.add('button-fx');
+
+    button.addEventListener('mouseout', () => {
+        button.classList.remove('button-fx');
+    });
 });
-
-button.addEventListener('mouseout', () => {
-    button.classList.remove('button-fx');
-});
-
-
 
 container.addEventListener('mouseover', () => {
     container.classList.add('container-fx');
-    
-});
 
-container.addEventListener('mouseout', () => {
-    container.classList.remove('container-fx');
+    container.addEventListener('mouseout', () => {
+        container.classList.remove('container-fx');
+    });
 });
-
-fullGrid();
 
 function createRow(input) {
     subContainer = document.createElement('div');
@@ -40,7 +35,7 @@ function createRow(input) {
 }
 
 function fullGrid() {
-    
+
     while (container.hasChildNodes()) {
         container.removeChild(container.lastChild)
     }
@@ -48,6 +43,8 @@ function fullGrid() {
     let userInput = prompt('Pick a number for the grid: ');
     if (userInput > 100) return fullGrid();
 
+    heading.textContent = `Here is your ${userInput} x ${userInput} grid!`
+    
     let myNewArray = [];
 
     for (i=1; i <= userInput; i++) {
@@ -57,6 +54,21 @@ function fullGrid() {
     myNewArray.forEach(() => {
         createRow(userInput);
     });
+
+    mouseOverFx();
+}
+
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color =  '#';
+
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function mouseOverFx() {
 
     const boxes = document.querySelectorAll('.box');
     
@@ -71,12 +83,4 @@ function fullGrid() {
     });
 }
 
-function getRandomColor() {
-    let letters = '0123456789ABCDEF';
-    let color =  '#';
-
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
+fullGrid();
